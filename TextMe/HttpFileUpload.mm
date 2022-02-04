5b804650d9e63bcdb855dd73cd1ca0dea2fc39a3
Sarfo Kantanka Frimpong <frimpongsarfok@gmail.com>
2022-02-03 19:44:29 -0500

    Not Needed 006

--- TextMe/HttpFileUpload.mm
+++
@@ -1,9 +0,0 @@
-//
-//  HttpFileUpload.cpp
-//  Nosy
-//
-//  Created by SARFO KANTANKA FRIMPONG on 4/27/19.
-//  Copyright © 2019 SARFO KANTANKA FRIMPONG. All rights reserved.
-//
-
-#include "HttpFileUpload.hpp"
--- TextMe/HttpFileUploadHandler.mm
+++
@@ -1,9 +0,0 @@
-//
-//  HttpFileUploadHandler.cpp
-//  Nosy
-//
-//  Created by SARFO KANTANKA FRIMPONG on 4/29/19.
-//  Copyright © 2019 SARFO KANTANKA FRIMPONG. All rights reserved.
-//
-
-#include "HttpFileUploadHandler.hpp"
